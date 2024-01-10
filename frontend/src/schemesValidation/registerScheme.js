140232815f742cd670d828ab18a4eac498a022b0
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-01-10 13:28:32 +0200

    Created schema validation for register form

--- frontend/src/schemesValidation/registerScheme.js
+++ frontend/src/schemesValidation/registerScheme.js
@@ -1,11 +1,11 @@
 import z from 'zod'
 
 export const registerScheme = z.object({
-  password:z.string().regex(/^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/`-]{8,}$/, "Error password"),
-  confirmPassword:z.string().regex(/^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/`-]{8,}$/, "Error password"),
   firstName:z.string().max(20).min(2).regex(/^[A-Z][a-zA-Z\s']+$/, "Error firstName"),
   lastName:z.string().max(20).min(2).regex(/^[A-Z][a-zA-Z\s']+$/, "Error lastName"),
-  email:z.string().email("Email is not valid!")
+  email:z.string().email("Email is not valid!"),
+  password:z.string().regex(/^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/`-]{8,}$/, "Error password"),
+  confirmPassword:z.string().regex(/^[a-zA-Z0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/`-]{8,}$/, "Error password"),
 }).refine((data)=>  data.password===data.confirmPassword,{
   message:"Passwords do not match",
   path:["confirmPassword"]
