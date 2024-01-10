f33e9309e29c4d098867440d28627abb6ad5ff71
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-01-10 14:09:06 +0200

    Created rtk query login endpoint

--- frontend/src/store/userApi.js
+++ frontend/src/store/userApi.js
@@ -4,9 +4,29 @@ import { BASE_URL } from "../utils/consts";
 export const userApi = createApi({
   reducerPath: "userApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: BASE_URL,
+    baseUrl: BASE_URL
   }),
   endpoints: (builder) => ({
+    login: builder.mutation({
+      query: (body) => {
+        return {
+          url: "/login",
+          method: "post",
+          body
+        };
+      }, transformResponse: (response, meta, arg) => {
+        localStorage.setItem("token", response.token);
+        return { ...response, meta };
+      }, transformErrorResponse: (error) => {
+        console.error("Login error:", error);
+
+        return {
+          error: true,
+          errorMessage: "Помилка при логіні"
+        };
+      }
+
+    }),
     registerUser: builder.mutation({
       query: (body) => {
         return {
@@ -15,8 +35,8 @@ export const userApi = createApi({
           body
         };
       }
-    }),
+    })
   })
 });
 
-export const {  useRegisterUserMutation } = userApi;
+export const { useLoginMutation, useRegisterUserMutation } = userApi;
