af47723728358f56685b6fd7454a826175d2b618
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-01-13 01:17:07 +0200

    Changed layout component

--- frontend/src/layouts/userLayout/UserLayout.jsx
+++ frontend/src/layouts/userLayout/UserLayout.jsx
@@ -1,16 +1,14 @@
 import { Outlet } from "react-router-dom";
 import s from "./UserLayout.module.css";
+import Header from "../../components/header/user/Header";
+import Footer from "../../components/footer/common/Footer";
 
 export default function UserLayout () {
   return <div className={s.container}>
-    <div>
-      HEADER
-    </div>
+    <Header />
     <div className={s.outlet}>
       <Outlet />
     </div>
-    <div>
-      FOOTER
-    </div>
+    <Footer />
   </div>;
 }
