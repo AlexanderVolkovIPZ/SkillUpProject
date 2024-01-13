83b6a7fc88db5438fc6f99f99297571409db2450
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-01-14 01:20:47 +0200

    Modernization of user header. Added + button for connecting and creating a course

--- frontend/src/components/header/user/Header.js
+++ frontend/src/components/header/user/Header.js
@@ -19,12 +19,14 @@ import MailIcon from "@mui/icons-material/Mail";
 import NotificationsIcon from "@mui/icons-material/Notifications";
 import MoreIcon from "@mui/icons-material/MoreVert";
 import AccountCircle from "@mui/icons-material/AccountCircle";
+import AddIcon from "@mui/icons-material/Add";
 import s from "./Header.module.css";
 
 export default function Header () {
 
   const { setAuthenticated } = useContext(AuthContext);
-  const [anchorEl, setAnchorEl] = useState(null);
+  const [anchorEl1, setAnchorEl1] = useState(null);
+  const [anchorEl2, setAnchorEl2] = useState(null);
   const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
   const navigate = useNavigate();
   const [logout, { data, isSuccess, isError, error }] = useLogoutMutation();
@@ -35,19 +37,27 @@ export default function Header () {
     navigate("/login");
   };
 
-  const isMenuOpen = Boolean(anchorEl);
+  const isMenuOpen1 = Boolean(anchorEl1);
+  const isMenuOpen2 = Boolean(anchorEl2);
   const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
 
-  const handleProfileMenuOpen = (event) => {
-    setAnchorEl(event.currentTarget);
+  const handleProfileMenuOpen1 = (event) => {
+    setAnchorEl1(event.currentTarget);
+  };
+
+  const handleProfileMenuOpen2 = (event) => {
+    setAnchorEl2(event.currentTarget);
   };
 
   const handleMobileMenuClose = () => {
     setMobileMoreAnchorEl(null);
   };
-
-  const handleMenuClose = () => {
-    setAnchorEl(null);
+  const handleMenuClose1 = () => {
+    setAnchorEl1(null);
+    handleMobileMenuClose();
+  };
+  const handleMenuClose2 = () => {
+    setAnchorEl2(null);
     handleMobileMenuClose();
   };
 
@@ -55,28 +65,59 @@ export default function Header () {
     setMobileMoreAnchorEl(event.currentTarget);
   };
 
-  const menuId = "primary-search-account-menu";
-  const renderMenu = (
+  const menuId1 = "primary-search-actions-menu";
+  const renderMenu1 = (
     <Menu
-      anchorEl={anchorEl}
+      anchorEl={anchorEl1}
       anchorOrigin={{
         vertical: "top",
         horizontal: "right"
       }}
-      id={menuId}
+      id={menuId1}
       keepMounted
       transformOrigin={{
         vertical: "top",
         horizontal: "right"
       }}
-      open={isMenuOpen}
-      onClose={handleMenuClose}
+      open={isMenuOpen1}
+      onClose={handleMenuClose1}
     >
-      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
-      <MenuItem onClick={handleMenuClose}>My account</MenuItem>
+      <Link to="/course/create" className={s.link}>
+        <MenuItem onClick={handleMenuClose1}>
+          Create course
+        </MenuItem>
+      </Link>
+
+      <Link to="/course/connect" className={s.link}>
+        <MenuItem onClick={handleMenuClose1}>
+          Connect to course
+        </MenuItem>
+      </Link>
+    </Menu>
+  );
+
+  const menuId2 = "primary-search-account-menu";
+  const renderMenu2 = (
+    <Menu
+      anchorEl={anchorEl2}
+      anchorOrigin={{
+        vertical: "top",
+        horizontal: "right"
+      }}
+      id={menuId2}
+      keepMounted
+      transformOrigin={{
+        vertical: "top",
+        horizontal: "right"
+      }}
+      open={isMenuOpen2}
+      onClose={handleMenuClose2}
+    >
+      <MenuItem onClick={handleMenuClose2}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose2}>My account</MenuItem>
       <MenuItem
         onClick={() => {
-          handleMenuClose();
+          handleMenuClose2();
           onHandleExit();
         }}
       >Exit</MenuItem>
@@ -120,11 +161,25 @@ export default function Header () {
         </IconButton>
         <p>Notifications</p>
       </MenuItem>
-      <MenuItem onClick={handleProfileMenuOpen}>
+
+      <MenuItem onClick={handleProfileMenuOpen1}>
+        <IconButton
+          size="large"
+          aria-label="actions current user"
+          aria-controls={menuId1}
+          aria-haspopup="true"
+          color="inherit"
+        >
+          <AddIcon />
+        </IconButton>
+        <p>Actions</p>
+      </MenuItem>
+
+      <MenuItem onClick={handleProfileMenuOpen2}>
         <IconButton
           size="large"
           aria-label="account of current user"
-          aria-controls="primary-search-account-menu"
+          aria-controls={menuId2}
           aria-haspopup="true"
           color="inherit"
         >
@@ -135,7 +190,8 @@ export default function Header () {
     </Menu>
   );
 
-  return (<Box style={{ position: "sticky", top: 0, zIndex: 1 }}>
+  return (
+    <Box style={{ position: "sticky", top: 0, zIndex: 1 }}>
       <AppBar position="static">
         <div className={s.toolbarWrapper}>
           <Toolbar>
@@ -157,6 +213,7 @@ export default function Header () {
             >
               USER
             </Typography>
+
             <Box sx={{ flexGrow: 1 }} />
             <Box sx={{ display: { xs: "none", md: "flex" } }}>
               <IconButton size="large" aria-label="show 4 new mails" color="inherit">
@@ -164,6 +221,7 @@ export default function Header () {
                   <MailIcon />
                 </Badge>
               </IconButton>
+
               <IconButton
                 size="large"
                 aria-label="show 17 new notifications"
@@ -173,18 +231,32 @@ export default function Header () {
                   <NotificationsIcon />
                 </Badge>
               </IconButton>
+
+              <IconButton
+                size="large"
+                edge="end"
+                aria-label="actions of current user"
+                aria-controls={menuId1}
+                aria-haspopup="true"
+                onClick={handleProfileMenuOpen1}
+                color="inherit"
+              >
+                <AddIcon />
+              </IconButton>
+
               <IconButton
                 size="large"
                 edge="end"
                 aria-label="account of current user"
-                aria-controls={menuId}
+                aria-controls={menuId2}
                 aria-haspopup="true"
-                onClick={handleProfileMenuOpen}
+                onClick={handleProfileMenuOpen2}
                 color="inherit"
               >
                 <AccountCircle />
               </IconButton>
             </Box>
+
             <Box sx={{ display: { xs: "flex", md: "none" } }}>
               <IconButton
                 size="large"
@@ -201,7 +273,8 @@ export default function Header () {
         </div>
       </AppBar>
       {renderMobileMenu}
-      {renderMenu}
+      {renderMenu1}
+      {renderMenu2}
     </Box>
   );
 }
