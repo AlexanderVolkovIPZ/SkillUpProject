bb68b87dea2da6fdeb1d0a8117748be0190a0e2a
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-01-14 23:54:29 +0200

    Modernization of UserLayout

--- frontend/src/layouts/userLayout/UserLayout.jsx
+++ frontend/src/layouts/userLayout/UserLayout.jsx
@@ -2,12 +2,17 @@ import { Outlet } from "react-router-dom";
 import s from "./UserLayout.module.css";
 import Header from "../../components/header/user/Header";
 import Footer from "../../components/footer/common/Footer";
+import TemporaryDrawer from "../../components/drawer/user/Drawer";
+
 
 export default function UserLayout () {
   return <div className={s.container}>
     <Header />
-    <div className={s.outlet}>
-      <Outlet />
+    <div className={s.wrapper}>
+      <TemporaryDrawer/>
+      <div className={s.outlet}>
+        <Outlet />
+      </div>
     </div>
     <Footer />
   </div>;
