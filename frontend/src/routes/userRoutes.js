e5cbb20ea591142d52d0146e24af495533b51876
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-01-16 02:15:17 +0200

    Added new router to userRoutes

--- frontend/src/routes/userRoutes.js
+++ frontend/src/routes/userRoutes.js
@@ -1,5 +1,10 @@
 import { guestRoutes } from "./guestRoutes";
+import { lazy } from "react";
+const CreateCoursePage = lazy(()=>import("../pages/course/createPage/CreateCoursePage"))
 
 export const userRoutes = [
-
+  {
+    path: "/course/create",
+    element: <CreateCoursePage/>
+  }
 ].concat(guestRoutes);
