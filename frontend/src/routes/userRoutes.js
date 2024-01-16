2766337d03fbefdac5b6a72b6601eeb9e33a0a4e
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-01-16 06:02:42 +0200

    Created new router for connect course page

--- frontend/src/routes/userRoutes.js
+++ frontend/src/routes/userRoutes.js
@@ -1,10 +1,16 @@
 import { guestRoutes } from "./guestRoutes";
 import { lazy } from "react";
-const CreateCoursePage = lazy(()=>import("../pages/course/createPage/CreateCoursePage"))
+
+const CreateCoursePage = lazy(() => import("../pages/course/createPage/CreateCoursePage"));
+const ConnectCoursePage = lazy(() => import("../pages/course/connectPage/ConnectCoursePage"));
 
 export const userRoutes = [
   {
     path: "/course/create",
-    element: <CreateCoursePage/>
+    element: <CreateCoursePage />
+  },
+  {
+    path: "/course/connect",
+    element: <ConnectCoursePage />
   }
 ].concat(guestRoutes);
