3b66b0bc12613ac40f2971398f6a0f6f53191ff3
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-01-17 06:58:23 +0200

    Added new custom controller for get all user's courses by user id

---
+++ api/src/Controller/CourseController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\CourseRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CourseController extends AbstractController
+{
+
+    private CourseRepository $courseRepository;
+
+    public function __construct(CourseRepository $courseRepository)
+    {
+        $this->courseRepository = $courseRepository;
+    }
+
+    #[Route("/api/user-courses", name: "courses_by_user")]
+    public function coursesByUserID(): JsonResponse
+    {
+        $userId = $this->getUser()->getId();
+
+        $courses = $this->courseRepository->findCoursesByUserId($userId);
+
+        return new JsonResponse($courses);
+    }
+
+}
