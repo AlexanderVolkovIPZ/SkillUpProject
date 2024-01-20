60c67a57a9ae605e8feac8d2fc26055377f34b1b
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-01-20 12:20:49 +0200

    Created temporary pages-plugs

---
+++ frontend/src/pages/course/marksSubPage/MarkSubPage.jsx
@@ -0,0 +1,10 @@
+import { Helmet } from "react-helmet";
+
+export default function MarkSubPage () {
+  return <div>
+    <Helmet>
+      <title>MarkSubPage</title>
+    </Helmet>
+    MarkSubPage працює
+  </div>;
+}
---
+++ frontend/src/pages/course/userSubPage/UserSubPage.jsx
@@ -0,0 +1,10 @@
+import { Helmet } from "react-helmet";
+
+export default function UserSubPage () {
+  return <div>
+    <Helmet>
+      <title>UserSubPage</title>
+    </Helmet>
+    UserSubPage працює
+  </div>;
+}
