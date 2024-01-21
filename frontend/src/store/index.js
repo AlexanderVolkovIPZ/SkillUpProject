67a014691c88251c5467b5aa99f9acf1a0f425cb
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-01-21 19:08:28 +0200

    Added new reducer and middleware

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,14 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import { userApi } from "./userApi";
 import { courseApi } from "./courseApi";
 import { courseUserApi } from "./courseUserApi";
-import { taskApi } from "./taksApi";
+import { taskApi } from "./taskApi";
+import { taskUserApi } from "./taskUserApi";
 
 export const store = configureStore({
   reducer: {
     [userApi.reducerPath]: userApi.reducer,
     [courseApi.reducerPath]: courseApi.reducer,
     [courseUserApi.reducerPath]: courseUserApi.reducer,
-    [taskApi.reducerPath]: taskApi.reducer
+    [taskApi.reducerPath]: taskApi.reducer,
+    [taskUserApi.reducerPath]: taskUserApi.reducer
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware).concat(courseApi.middleware).concat(courseUserApi.middleware).concat(taskApi.middleware)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(userApi.middleware).concat(courseApi.middleware).concat(courseUserApi.middleware).concat(taskApi.middleware).concat(taskUserApi.middleware)
 });
