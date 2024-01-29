7bdf4085e91d86645ae2c87d703a50d2acd70f20
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-01-29 13:29:17 +0200

    Added uploads data

--- frontend/src/pages/task/taskPage/user/TaskPage.jsx
+++ frontend/src/pages/task/taskPage/user/TaskPage.jsx
@@ -7,16 +7,17 @@ import { attachTaskSolutionSchema } from "../../../../schemesValidation/attachTa
 import { AuthContext } from "../../../../context/authContext";
 import { useDeleteTaskUserMutation, useGetTaskUserQuery } from "../../../../store/taskUserApi";
 import { useIsCurrentUserCourseCreatorQuery } from "../../../../store/courseApi";
+import { TaskDataContext } from "../../../../context/taskDataContext";
 import {
   Avatar,
   Box, Button,
   IconButton,
   Typography
 } from "@mui/material";
-
 import CircularProgressModal from "../../../../components/circularProgressModal/CircularProgressModal";
 import FileViewer from "../../../../components/fileViewer/FileViewer";
 import FormAttachTask from "../../../../components/modalAttachTask/user/FormAttachTask";
+import UpdateTaskDialog from "../../../../components/updateTaskDialog/user/UpdateTaskDialog";
 import Divider from "@mui/material/Divider";
 import AssignmentIcon from "@mui/icons-material/Assignment";
 import List from "@mui/material/List";
@@ -27,7 +28,6 @@ import FolderIcon from "@mui/icons-material/Folder";
 import NotesIcon from "@mui/icons-material/Notes";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import CancelScheduleSendIcon from "@mui/icons-material/CancelScheduleSend";
-import UpdateIcon from "@mui/icons-material/Update";
 import { deepOrange } from "@mui/material/colors";
 import s from "./TaskPage.module.css";
 
@@ -92,13 +92,13 @@ export default function TaskPage () {
   const handleDeleteSubmit = async () => {
     const taskUserId = submittedTask["@id"]?.split("/")?.pop();
     try {
-      await deleteTaskUser(taskUserId);
       setFileNameSubmitted(null);
       setLinkSubmitted(null);
       setFileName(null);
       setLink(null);
       reset();
       setStatus("Appointed");
+      await deleteTaskUser(taskUserId);
     } catch (error) {
       console.log(error);
     }
@@ -109,113 +109,111 @@ export default function TaskPage () {
       {isLoadingTask || isLoadingTaskUser || isUserCreatorData ? (
         <CircularProgressModal />
       ) : (
-        <Box className={s.taskBox}>
-          <Box className={s.leftSide}>
-            <Avatar sx={{ bgcolor: deepOrange[500] }}>
-              <AssignmentIcon />
-            </Avatar>
-            <Box className={s.boxInfo}>
-              <Typography variant="h4">{taskData?.name}</Typography>
-              <Typography variant="overline" className={s.date}>{taskData?.createdAt}</Typography>
-              <Divider color="warning" />
-              {taskData?.description ? <Box className={s.boxDescription}>
-                <Box className={s.descriptionBoxIconLabel}>
-                  <NotesIcon color="primary" />
-                  <Typography variant="h5" className={s.descriptionLbl}>Description</Typography>
-                </Box>
-                <Typography variant="body1" dangerouslySetInnerHTML={{ __html: taskData?.description }}></Typography>
-              </Box> : null}
-              {taskData?.fileNameTask ? <Box className={s.boxFileContent}>
-                <Box className={s.filesBoxIconLabel}>
-                  <FolderIcon color="primary" />
-                  <Typography variant="h5">Files</Typography>
-                </Box>
-                <List>
-                  <FileViewer fileName={taskData?.fileNameTask} />
-                </List>
-              </Box> : null}
-            </Box>
-          </Box>
-          <Box className={s.rightSide}>
-            <Box className={s.yourWork}>
-              {!userCreatorData?.isUserCourseCreator ? (
-                <>
-                  <Box className={s.titleAttachTask}>
-                    <Typography variant="h6">Your task</Typography>
-                    <Typography variant="overline" className={s.statusLabel}>{status}</Typography>
+        <TaskDataContext.Provider
+          value={{
+            taskData: taskData
+          }}
+        >
+          <Box className={s.taskBox}>
+            <Box className={s.leftSide}>
+              <Avatar sx={{ bgcolor: deepOrange[500] }}>
+                <AssignmentIcon />
+              </Avatar>
+              <Box className={s.boxInfo}>
+                <Typography variant="h4">{taskData?.name}</Typography>
+                <Typography variant="overline" className={s.date}>{taskData?.createdAt}</Typography>
+                <Divider color="warning" />
+                {taskData?.description ? <Box className={s.boxDescription}>
+                  <Box className={s.descriptionBoxIconLabel}>
+                    <NotesIcon color="primary" />
+                    <Typography variant="h5" className={s.descriptionLbl}>Description</Typography>
                   </Box>
-                  {fileNameSubmitted && (
-                    <Box className={`${s.fileNameBoxAttachTask} ${s.itemTaskSubmitted}`}>
-                      <Box className={s.fileNameWithIcon}>
-                        <TextSnippetIcon />
-                        <Typography className={s.fileName}>{fileNameSubmitted}</Typography>
-                      </Box>
+                  <Typography variant="body1" dangerouslySetInnerHTML={{ __html: taskData?.description }}></Typography>
+                </Box> : null}
+                {taskData?.fileNameTask ? <Box className={s.boxFileContent}>
+                  <Box className={s.filesBoxIconLabel}>
+                    <FolderIcon color="primary" />
+                    <Typography variant="h5">Files</Typography>
+                  </Box>
+                  <List>
+                    <FileViewer fileName={taskData?.fileNameTask} />
+                  </List>
+                </Box> : null}
+              </Box>
+            </Box>
+            <Box className={s.rightSide}>
+              <Box className={s.yourWork}>
+                {!userCreatorData?.isUserCourseCreator ? (
+                  <>
+                    <Box className={s.titleAttachTask}>
+                      <Typography variant="h6">Your task</Typography>
+                      <Typography variant="overline" className={s.statusLabel}>{status}</Typography>
                     </Box>
-                  )}
-                  {linkSubmitted && (
-                    <Box className={`${s.linkBoxAttachTask} ${s.itemTaskSubmitted}`}>
-                      <Box className={s.linkWithIcon}>
-                        <LinkIcon />
-                        <Typography className={s.linkName}>{linkSubmitted}</Typography>
+                    {fileNameSubmitted && (
+                      <Box className={`${s.fileNameBoxAttachTask} ${s.itemTaskSubmitted}`}>
+                        <Box className={s.fileNameWithIcon}>
+                          <TextSnippetIcon />
+                          <Typography className={s.fileName}>{fileNameSubmitted}</Typography>
+                        </Box>
                       </Box>
-                    </Box>
-                  )}
-                  {fileName && !fileNameSubmitted && (
-                    <Box className={s.fileNameBoxAttachTask}>
-                      <Box className={s.fileNameWithIcon}>
-                        <TextSnippetIcon />
-                        <Typography className={s.fileName}>{fileName}</Typography>
+                    )}
+                    {linkSubmitted && (
+                      <Box className={`${s.linkBoxAttachTask} ${s.itemTaskSubmitted}`}>
+                        <Box className={s.linkWithIcon}>
+                          <LinkIcon />
+                          <Typography className={s.linkName}>{linkSubmitted}</Typography>
+                        </Box>
                       </Box>
-                      <IconButton aria-label="delete" size="large" onClick={handleRemoveFile}>
-                        <ClearIcon />
-                      </IconButton>
-                    </Box>
-                  )}
-                  {link && !linkSubmitted && (
-                    <Box className={s.linkBoxAttachTask}>
-                      <Box className={s.linkWithIcon}>
-                        <LinkIcon />
-                        <Typography className={s.linkName}>{link}</Typography>
+                    )}
+                    {fileName && !fileNameSubmitted && (
+                      <Box className={s.fileNameBoxAttachTask}>
+                        <Box className={s.fileNameWithIcon}>
+                          <TextSnippetIcon />
+                          <Typography className={s.fileName}>{fileName}</Typography>
+                        </Box>
+                        <IconButton aria-label="delete" size="large" onClick={handleRemoveFile}>
+                          <ClearIcon />
+                        </IconButton>
                       </Box>
-                      <IconButton aria-label="delete" size="large" onClick={handleRemoveLink}>
-                        <ClearIcon />
-                      </IconButton>
-                    </Box>
-                  )}
-                  <FormAttachTask setFileName={setFileName} setLink={setLink} register={register} handleSubmit={handleSubmit} taskId={taskId} isSubmittedFile={fileNameSubmitted !== null} isSubmittedLink={linkSubmitted !== null} />
-                  {(fileNameSubmitted || linkSubmitted) && (
+                    )}
+                    {link && !linkSubmitted && (
+                      <Box className={s.linkBoxAttachTask}>
+                        <Box className={s.linkWithIcon}>
+                          <LinkIcon />
+                          <Typography className={s.linkName}>{link}</Typography>
+                        </Box>
+                        <IconButton aria-label="delete" size="large" onClick={handleRemoveLink}>
+                          <ClearIcon />
+                        </IconButton>
+                      </Box>
+                    )}
+                    <FormAttachTask setFileName={setFileName} setLink={setLink} register={register} handleSubmit={handleSubmit} taskId={taskId} isSubmittedFile={fileNameSubmitted !== null} isSubmittedLink={linkSubmitted !== null} />
+                    {(fileNameSubmitted || linkSubmitted) && (
+                      <Button
+                        fullWidth variant="outlined" endIcon={<CancelScheduleSendIcon />} onClick={handleDeleteSubmit}
+                      >
+                        Cancel sending
+                      </Button>
+                    )}
+                  </>
+                ) : (
+                  <>
+                    <UpdateTaskDialog />
                     <Button
-                      fullWidth variant="outlined" endIcon={<CancelScheduleSendIcon />} onClick={handleDeleteSubmit}
+                      fullWidth
+                      variant="outlined"
+                      endIcon={<DeleteForeverIcon />}
+                      component={Link}
+                      to="delete"
                     >
-                      Cancel sending
+                      Delete task
                     </Button>
-                  )}
-                </>
-              ) : (
-                <>
-                  <Button
-                    fullWidth
-                    variant="outlined"
-                    endIcon={<UpdateIcon />}
-                    component={Link}
-                    to="update"
-                  >
-                    Update task
-                  </Button>
-                  <Button
-                    fullWidth
-                    variant="outlined"
-                    endIcon={<DeleteForeverIcon />}
-                    component={Link}
-                    to="delete"
-                  >
-                    Delete task
-                  </Button>
-                </>
-              )}
+                  </>
+                )}
+              </Box>
             </Box>
           </Box>
-        </Box>
+        </TaskDataContext.Provider>
       )}
     </>
   );
