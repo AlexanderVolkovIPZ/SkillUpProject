6bb73bc3d8dce2cdc568fafb0de722af00eb12a9
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-02-02 15:55:08 +0200

    Created MarkSubPage component

--- frontend/src/pages/course/marksSubPage/MarkSubPage.jsx
+++ frontend/src/pages/course/marksSubPage/MarkSubPage.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
+import MarkTable from "../../../components/markTable/user/MarkTable";
+import { Alert } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
+import s from "./MarkSubPage.module.css";
 
-export default function MarkSubPage () {
-  return <div>
+export default function MarkSubPage ({ setValueTab }) {
+  const [isSuccessfulUpdateMark, setIsSuccessfulUpdateMark] = useState(false);
+
+  useEffect(() => {
+    setValueTab("4");
+  }, []);
+
+  return <>
     <Helmet>
       <title>MarkSubPage</title>
     </Helmet>
-    MarkSubPage працює
-  </div>;
+    {isSuccessfulUpdateMark &&
+      <Alert icon={<CheckIcon fontSize="inherit" />} severity="success" className={s.successfullyUpdatedMark}>
+        Mark update successfully.
+      </Alert>}
+    <MarkTable setIsSuccessfulUpdateMark={setIsSuccessfulUpdateMark} />
+  </>;
 }
