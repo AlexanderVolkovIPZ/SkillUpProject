a3f8521f4de5038f3a470ff32ba2604bd0d6a297
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-02-03 20:16:19 +0200

    Added alert for successful course connecting

--- frontend/src/pages/course/connectPage/ConnectCoursePage.jsx
+++ frontend/src/pages/course/connectPage/ConnectCoursePage.jsx
@@ -1,14 +1,23 @@
 import { Helmet } from "react-helmet";
 import ConnectCourseForm from "../../../components/connectCourseForm/user/ConnectCourseForm";
-import { Typography } from "@mui/material";
+import { Alert, Typography } from "@mui/material";
 import s from "./ConnectCoursePage.module.css";
+import { useState } from "react";
+import CheckIcon from "@mui/icons-material/Check";
 
 export default function ConnectCoursePage () {
+
+  const [isCourseSuccessfullyConnected, setIsCourseSuccessfullyConnected] = useState(false);
 
   return <div className={s.connectCoursePage}>
     <Helmet>
       <title>Connect course</title>
     </Helmet>
+    {isCourseSuccessfullyConnected && <Alert
+      className={s.taskCourseConnectSuccessfullyAlert} icon={<CheckIcon fontSize="inherit" />} severity="success"
+    >
+      You connected successfully!
+    </Alert>}
     <div className={s.labelsWrapper}>
       <Typography variant="h5" gutterBottom>
         Course code
@@ -17,6 +26,6 @@ export default function ConnectCoursePage () {
         Enter the course code (you can find it out from the teacher).
       </Typography>
     </div>
-    <ConnectCourseForm />
-  </div>
+    <ConnectCourseForm setIsCourseSuccessfullyConnected={setIsCourseSuccessfullyConnected} />
+  </div>;
 }
