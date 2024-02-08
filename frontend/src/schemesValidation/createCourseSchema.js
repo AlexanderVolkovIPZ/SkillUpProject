75c7bd396238bf48ecdcc1d4032f904d596637f1
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-02-08 17:32:44 +0200

    Changed schemas validation config, added defalult value

--- frontend/src/schemesValidation/createCourseSchema.js
+++ frontend/src/schemesValidation/createCourseSchema.js
@@ -2,6 +2,6 @@ import z from 'zod'
 
 export const createCourseSchema = z.object({
   name:z.string().min(5).max(70).nonempty("This field can't be empty!"),
-  title:z.string().max(100).nullable(),
-  description:z.string().max(2000).nullable(),
+  title:z.string().max(100).nullable().default(""),
+  description:z.string().max(2000).nullable().default(""),
 })
--- frontend/src/schemesValidation/updateTaskSchema.js
+++ frontend/src/schemesValidation/updateTaskSchema.js
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const updateTaskSchema = z.object({
   name: z.string().min(5).max(70).nonempty("This field can't be empty!"),
   description: z.string().max(10000).nullable().default(""),
-  date: z.date().nullable().default(null),
+  date: z.date().nullable().default(new Date()),
   mark: z.number().min(0).max(100).default(0),
   file: z.any().nullable().default(null)
 });
