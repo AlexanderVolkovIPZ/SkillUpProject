f14e1f008cd0027bad1bfea29f794ad8349f29bc
Alexander Volkov <volkov.aliksandr0203@gmail.com>
2024-02-08 19:20:50 +0200

    Added a notification component about a successfully created course

--- frontend/src/pages/course/createPage/CreateCoursePage.jsx
+++ frontend/src/pages/course/createPage/CreateCoursePage.jsx
@@ -1,13 +1,23 @@
 import CreateCourseForm from "../../../components/createCourseForm/user/CreateCourseForm";
 import { Helmet } from "react-helmet";
-import s from "./CreateCoursePage.module.css"
+import s from "./CreateCoursePage.module.css";
+import { useState } from "react";
+import { Alert } from "@mui/material";
+import CheckIcon from "@mui/icons-material/Check";
 
-export default function CreateCoursePage(){
+export default function CreateCoursePage () {
+
+  const [isCourseSuccessfullyCreated, setIsCourseSuccessfullyCreated] = useState(false);
 
   return <>
     <Helmet>
       <title>Create course</title>
     </Helmet>
-    <CreateCourseForm />
+    {isCourseSuccessfullyCreated && <Alert
+      className={s.taskCourseCreatedSuccessfullyAlert} icon={<CheckIcon fontSize="inherit" />} severity="success"
+    >
+      New course created successfully!
+    </Alert>}
+    <CreateCourseForm setIsCourseSuccessfullyCreated={setIsCourseSuccessfullyCreated} />
   </>
 }
